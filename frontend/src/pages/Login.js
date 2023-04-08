71c6642a43533381df99f2685b612c293cab9757
khushdj2002 <108506287+khushdj2002@users.noreply.github.com>
2023-04-08 17:27:14 +0530

    Login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import '../Styles/login.css'
 const Login = () => {
   return (
-    <div>Login
-      
+    <div id='header'>
+      <h3><b>Login</b> or <b>Signup</b></h3>
+      <form> 
+        
+        <input type='text' name='pNumber' placeholder='+91 | Mobile Number *' required></input>
+        <h6>By continuing, I agree to the<br></br> <b className='terms'>Terms of Use </b>&<b className='terms'> Privacy Policy</b></h6>
+        <button type="submit" class="btn btn2">CONTINUE</button>
+        <h6>Have trouble logging in? <b className='terms'>Get help</b></h6>
+      </form>
     </div>
   )
 }
