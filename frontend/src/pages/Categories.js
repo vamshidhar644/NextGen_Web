fadab3cf0f353c8efb5fae50a588ca2b2a3493c8
khushdj2002 <108506287+khushdj2002@users.noreply.github.com>
2023-04-09 00:40:23 +0530

    categories

---
+++ frontend/src/pages/Categories.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import '../Styles/Categories.css'
+const Categories = () => {
+  return (
+    <div>
+        <div id="services">
+            <div class="container">
+                <h1 class="sub-title">Categories</h1>
+                <div class="services-list">
+                    <div> 
+                        <i class="fa-solid fa-code"></i>  
+                        <h2>A</h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor quas voluptates corporis quia, vero quis pariatur hic, accusamus earum fugit cumque! Inventore hic asperiores fugiat ex aspernatur corporis, illum distinctio!</p>
+                        
+                    </div>
+                    <div> 
+                        <i class="fa-solid fa-crop"></i>  
+                        <h2>B</h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor quas voluptates corporis quia, vero quis pariatur hic, accusamus earum fugit cumque! Inventore hic asperiores fugiat ex aspernatur corporis, illum distinctio!</p>
+                        
+                    </div>
+                    <div>   
+                        <i class="fa-brands fa-app-store"></i>
+                        <h2>C</h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor quas voluptates corporis quia, vero quis pariatur hic, accusamus earum fugit cumque! Inventore hic asperiores fugiat ex aspernatur corporis, illum distinctio!</p>
+                        
+                    </div>
+                    <div>   
+                        <i class="fa-brands fa-app-store"></i>
+                        <h2>D</h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor quas voluptates corporis quia, vero quis pariatur hic, accusamus earum fugit cumque! Inventore hic asperiores fugiat ex aspernatur corporis, illum distinctio!</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Categories
