a5bd2b70d86fac3471b150704195b14f2887305f
Evilsheart <naveen.20bci7096@vitap.ac.in>
2023-04-08 19:39:02 +0530

    changes

---
+++ frontend/src/Components/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
+
+const Banner = () => {
+  return (
+    <div>
+        Carousel
+    </div>
+  )
+}
+
+export default Banner
