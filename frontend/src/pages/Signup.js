917d6017afbf11127de84f762444a8c0ea9ee1c6
khushdj2002 <108506287+khushdj2002@users.noreply.github.com>
2023-04-08 22:35:34 +0530

    SignUp

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -3,8 +3,12 @@ import { useSignup } from '../hooks/useSignup';
 import { Link, useNavigate } from 'react-router-dom';
 
 const Signup = () => {
+  const [fName, setfName]=useState('');
+  const [lName, setlName]=useState('');
   const [email, setEmail] = useState('');
+  const [pNumber, setPnumber] = useState('');
   const [password, setPassword] = useState('');
+  const [Cpassword, setCpassword] = useState('');
   const { signup, error, isLoading } = useSignup();
 
   const navigate = useNavigate();
@@ -19,18 +23,52 @@ const Signup = () => {
       <form className="login-form">
         <span className="signup">Signup</span>
         <input
+          type="text"
+          placeholder="First Name *"
+          onChange={(e) => setfName(e.target.value)}
+          value={fName}
+          className="form--input"
+          required
+        />
+        <input
+          type="text"
+          placeholder="Last Name *"
+          onChange={(e) => setlName(e.target.value)}
+          value={lName}
+          className="form--input"
+          required
+        />
+        <input
           type="email"
-          placeholder="Email address"
+          placeholder="Email address *"
           onChange={(e) => setEmail(e.target.value)}
           value={email}
           className="form--input"
+          required
+        />
+        <input
+          type="text"
+          placeholder="Phone Number *"
+          onChange={(e) => setPnumber(e.target.value)}
+          value={pNumber}
+          className="form--input"
+          required
         />
         <input
           type="password"
-          placeholder="Password"
+          placeholder="Password *"
           onChange={(e) => setPassword(e.target.value)}
           value={password}
           className="form--input"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password *"
+          onChange={(e) => setCpassword(e.target.value)}
+          value={Cpassword}
+          className="form--input"
+          required
         />
         {error && <div className="error">{error}</div>}
 
